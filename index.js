e11517dc8e303e5de27c673daf1bdcfbdf087ad5
Tegas Aziz <49786146+WilloIzCitron@users.noreply.github.com>
2021-03-15 08:18:33 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+<html>
+
+<head>
+<link rel="stylesheet" href="src/general/body.css">
+
+<link rel="stylesheet" href="src/dark/btn.css">
+<link rel="stylesheet" href="src/dark/hdr.css">
+<link rel="stylesheet" href="src/dark/ftr.css">
+
+<link rel="stylesheet" href="src/blue/btn.css">
+<link rel="stylesheet" href="src/blue/hdr.css">
+<link rel="stylesheet" href="src/blue/ftr.css">
+
+
+
+</head>
+<body id="body">
+<div class="hdrDarkBrelative">
+hewo
+</div><p>
+<font size = 55>HEWO WORLD <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>wertyuio</font>
+<div id="btnToTopOrangeBcircle">
+hewo
+</div>
+
+</body></html>
